5103aef123f17e84e5442708f9baf2a8820dd3ec
SureshSingh80 <kumarsuresh8403@gmail.com>
2025-11-23 13:43:16 +0530

    encryption and decryption feature completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,98 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import CryptoJS from "crypto-js";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [key, setKey] = useState("");
+  const [selected, setSelected] = useState("Encrypt");
+  const [output, setOutput] = useState("");
+
+  const handleCryptoGrahpy = () => {
+    if (text === "" || key === "") return;
+    if (selected === "Encrypt") {
+      const encryptedText = CryptoJS.AES.encrypt(text, key).toString();
+      // console.log(encryptedText);
+      setOutput(encryptedText);
+    } else {
+      const decryptedText = CryptoJS.AES.decrypt(text, key).toString(
+        CryptoJS.enc.Utf8
+      );
+      // console.log(decryptedText);
+      setOutput(decryptedText);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div
+      style={{
+        backgroundImage:
+          "url('https://www.logsign.com/uploads/encryption_ff2b4dd67c.jpg')",
+      }}
+      className="bg-gray-50 min-h-screen text-black flex items-center justify-center rounded-xl"
+    >
+      <div className="bg-white p-8 rounded shadow">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Welcome to the CryptoJS Decryption Tool
+        </h1>
+        <p className="text-base mb-4">
+          This tool allows you to decrypt and encrypted messages using the
+          CryptoJS library.
+        </p>
+        {/* input box */}
+        <div className="p-4">
+          <input
+            type="text"
+            value={text}
+            required
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter Text fro encryption or Decryption"
+            className="p-2 w-full border-2 border-gray-300 rounded focus:border-blue-400 focus:outline-none focus:ring-0"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        {/* Key */}
+        <div className="p-4">
+          <input
+            type="text"
+            value={key}
+            required
+            onChange={(e) => setKey(e.target.value)}
+            placeholder="Enter Key"
+            className="p-2 w-full border-2 border-gray-300 rounded focus:border-blue-400 focus:outline-none focus:ring-0"
+          />
         </div>
-      </main>
+
+        {/* selector */}
+        <div className="p-4">
+          <select
+            className="p-2 w-full border-2 border-gray-300 rounded 
+              focus:border-blue-400
+              focus:outline-none 
+              focus:ring-0 
+              active:outline-none 
+              active:ring-0 "
+              onChange={(e) => setSelected(e.target.value)}
+          >
+            <option value="Encrypt">Encrypt</option>
+            <option value="Decrypt">Decrypt</option>
+          </select>
+        </div>
+
+        {/* button */}
+        <div onClick={handleCryptoGrahpy} className="p-4 mt-8">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full ">
+            {selected}
+          </button>
+        </div>
+
+        {/* output */}
+        <div>
+          <h2 className="text-xl font-bold mb-4 text-center">Output:</h2>
+          <p className="text-center">{output}</p>
+        </div>
+      </div>
     </div>
   );
 }
